acc5c2cf5888b8e43a5b7dc6d2f9e7a134211727
Josh Walawender <jmwalawender@gmail.com>
2021-03-02 11:28:42 -1000

    Add CMOS detector

--- odl/detector_config.py
+++ odl/detector_config.py
@@ -214,3 +214,78 @@ class CCDDetectorConfig(DetectorConfig):
                          + self.erase_time()
         self.exptime = target/self.nexp - overhead_per_exp
         self.set_name()
+
+
+##-------------------------------------------------------------------------
+## CMOSDetectorConfig
+##-------------------------------------------------------------------------
+class CMOSDetectorConfig(DetectorConfig):
+    '''An object to hold information about a visible light detector
+    configuration.  This is an abstract class which we expect to be subclassed
+    to a particular instrument/detector.
+
+    Attributes
+    ----------
+    readoutmode : str
+        Either 'fast' or 'slow'
+
+    gain : int
+        The gain value.
+
+    binning : str
+        The binning, parsed as (nrows)x(ncolumns)
+
+    window : str
+        The window, parsed as x1:x2,y1:y2
+    '''
+    def __init__(self, instrument='GenericCMOS', detector='',
+                 exptime=0, nexp=1, readoutmode='fast', gain=1, overhead=0,
+                 binning='1x1', window=None):
+        super().__init__(instrument=instrument, detector=detector,
+                         exptime=exptime, nexp=nexp, readoutmode=readoutmode)
+        self.binning = binning
+        self.window = window
+        self.gain = gain
+        self.overhead = overhead
+        self.set_name()
+
+
+    def set_name(self):
+        self.name = f'{self}{self.detector} {self.exptime:.0f}s (gain {self.gain}) x{self.nexp}'
+
+
+    def to_header(self):
+        h = fits.Header()
+        h['DCNAME'] = (self.name, 'Detector Config Name')
+        h['DCINSTR'] = (self.instrument, 'Detector Config Instrument Name')
+        h['DCDET'] = (self.detector, 'Detector Config Detector Name')
+        h['DCEXPT'] = (self.exptime, 'Detector Config Exptime (sec)')
+        h['DCNEXP'] = (self.nexp, 'Detector Config Number of Exposures')
+        h['DCRDMODE'] = (self.readoutmode, 'Detector Config Readout Mode')
+        h['DCBIN'] = (self.binning, 'Detector Config Binning')
+        h['DCWINDOW'] = (self.window, 'Detector Config Window')
+        h['DCGAIN'] = (self.gain, 'Detector Config Gain')
+        return h
+
+
+    def to_dict(self):
+        output = super().to_dict()
+        output['gain'] = self.gain
+        output['binning'] = self.binning
+        output['window'] = self.window
+        return output
+
+
+    def estimate_duration(self):
+        total_time = self.overhead + self.exptime
+        return total_time*self.nexp
+
+
+    def match_time(self, target):
+        overhead_per_exp = self.other_overhead()\
+                         + self.readout_time()\
+                         + self.erase_time()
+        self.exptime = target/self.nexp - overhead_per_exp
+        self.set_name()
+
+
